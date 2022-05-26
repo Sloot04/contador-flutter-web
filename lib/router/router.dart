5a5101eb0c67df99ec3d23042b10a1d734f678a4
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-26 10:56:49 -0300

    fluro

---
+++ lib/router/router.dart
@@ -0,0 +1,18 @@
+import 'package:bases_web/ui/views/counter_view.dart';
+import 'package:fluro/fluro.dart';
+
+class Flurorouter {
+  static final FluroRouter router = FluroRouter();
+
+  static void configureRoutes() {
+    router.define(
+      '/',
+      handler: _counterHandler,
+    );
+  }
+
+  // Handlers
+  static final Handler _counterHandler= Handler(
+    handlerFunc: (  context, params)=> const CounterView()
+    );
+}
