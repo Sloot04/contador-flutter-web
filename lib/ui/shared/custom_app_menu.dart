da15dbe354f9d824ce8f9706f791b627d0d7d83c
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-25 21:43:21 -0300

    Menu responsivo

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -8,6 +8,19 @@ class CustomAppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, BoxConstraints constraints) {
+        return (constraints.maxWidth > 520)
+            ? _TableDesktopMenu()
+            : _MobileMenu();
+      },
+    );
+  }
+}
+
+class _TableDesktopMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       width: double.infinity,
@@ -16,19 +29,50 @@ class CustomAppMenu extends StatelessWidget {
           text: 'Contador Sateful',
           // onPressed: () =>Navigator.pushNamed(context, '/sateful'),
           //onPressed: () => navigationService.navigateTo('/sateful'),
-          onPressed: () =>locator<NavigationService>().navigateTo('/sateful'),
+          onPressed: () => locator<NavigationService>().navigateTo('/sateful'),
           color: Colors.black,
         ),
         const SizedBox(width: 10),
         CustomFlatButton(
           text: 'Contador Provider',
-          onPressed: () =>locator<NavigationService>().navigateTo('/provider'),
+          onPressed: () => locator<NavigationService>().navigateTo('/provider'),
           color: Colors.black,
         ),
         const SizedBox(width: 10),
         CustomFlatButton(
           text: 'Otra página',
-          onPressed: () =>locator<NavigationService>().navigateTo('/abc123'),
+          onPressed: () => locator<NavigationService>().navigateTo('/abc123'),
+          color: Colors.black,
+        ),
+      ]),
+    );
+  }
+}
+
+class _MobileMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        CustomFlatButton(
+          text: 'Contador Sateful',
+          onPressed: () => locator<NavigationService>().navigateTo('/sateful'),
+          color: Colors.black,
+        ),
+        const SizedBox(width: 10),
+        CustomFlatButton(
+          text: 'Contador Provider',
+          onPressed: () => locator<NavigationService>().navigateTo('/provider'),
+          color: Colors.black,
+        ),
+        const SizedBox(width: 10),
+        CustomFlatButton(
+          text: 'Otra página',
+          onPressed: () => locator<NavigationService>().navigateTo('/abc123'),
           color: Colors.black,
         ),
       ]),
